4533f7359ed449b482bd6f296ee8157caf7535e7
JunwooPark <johnnypark0201@gmail.com>
2018-08-02 09:40:41 +0900

    annotate buttonCommand

--- src/main/java/bukkit/org/scriptcraftjs/bukkit/ScriptCraftPlugin.java
+++ src/main/java/bukkit/org/scriptcraftjs/bukkit/ScriptCraftPlugin.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.Bukkit;
 
-import org.scriptcraftjs.bukkit.buttonCommand;
+//import org.scriptcraftjs.bukkit.buttonCommand;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
